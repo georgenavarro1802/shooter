c01a622df97acf6d33b70f93da7b1150926f8222
George <gnavarrog1802@gmail.com>
2022-08-02 21:03:21 -0400

    Grenades

--- main.py
+++ main.py
@@ -21,11 +21,15 @@ GRAVITY = 0.75
 moving_left = False
 moving_right = False
 shoot = False
-
+grenade = False
+grenade_thrown = False
 
 # load images
 # bullet
 bullet_img = pygame.image.load('img/icons/bullet.png').convert_alpha()
+# grenade
+grenade_img = pygame.image.load('img/icons/grenade.png').convert_alpha()
+
 
 # define colours
 BG = (144, 201, 120)
@@ -39,7 +43,7 @@ def draw_bg():
 
 class Soldier(pygame.sprite.Sprite):
 
-    def __init__(self, char_type, x, y, scale, speed, ammo):
+    def __init__(self, char_type, x, y, scale, speed, ammo, grenades):
         pygame.sprite.Sprite.__init__(self)
         self.alive = True
         self.char_type = char_type
@@ -47,6 +51,7 @@ class Soldier(pygame.sprite.Sprite):
         self.ammo = ammo
         self.start_ammo = ammo
         self.shoot_cooldown = 0
+        self.grenades = grenades
         self.health = 100
         self.max_health = self.health
         self.direction = 1  # -1: left, 1: rigth
@@ -188,18 +193,52 @@ class Bullet(pygame.sprite.Sprite):
             if player.alive:
                 player.health -= 5
                 self.kill()
+
         if pygame.sprite.spritecollide(enemy, bullet_group, False):
             if enemy.alive:
                 enemy.health -= 25
                 self.kill()
 
 
+class Grenade(pygame.sprite.Sprite):
+
+    def __init__(self, x, y, direction):
+        pygame.sprite.Sprite.__init__(self)
+        self.timer = 100
+        self.vel_y = -11
+        self.speed = 7
+        self.image = grenade_img
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+        self.direction = direction
+
+    def update(self):
+        self.vel_y += GRAVITY
+        dx = self.direction * self.speed
+        dy = self.vel_y
+
+        # chech collition with floor
+        if self.rect.bottom + dy > 300:
+            dy = 300 - self.rect.bottom
+            self.speed = 0
+
+        # check collition with walls
+        if self.rect.left + dx < 0 or self.rect.right + dx > SCREEN_WIDTH:
+            self.direction *= -1
+            dx = self.direction * self.speed
+
+        # update grenade position
+        self.rect.x += dx
+        self.rect.y += dy
+
+
 # create sprite groups
 bullet_group = pygame.sprite.Group()
+grenade_group = pygame.sprite.Group()
 
 
-player = Soldier('player', 200, 200, 3, 5, 20)
-enemy = Soldier('enemy', 400, 200, 3, 5, 20)
+player = Soldier('player', 200, 200, 3, 5, 20, 5)
+enemy = Soldier('enemy', 400, 200, 3, 5, 20, 0)
 
 
 run = True
@@ -218,12 +257,22 @@ while run:
     # update and draw groups
     bullet_group.update()
     bullet_group.draw(screen)
+    grenade_group.update()
+    grenade_group.draw(screen)
 
     # update player actions
     if player.alive:
         # shoot bullets
         if shoot:
             player.shoot()
+        # shoot grenades
+        elif grenade and not grenade_thrown and player.grenades > 0:
+            grenade = Grenade(player.rect.centerx + (0.5 * player.rect.size[0] * player.direction), player.rect.top, player.direction)
+            grenade_group.add(grenade)
+            # reduce grenades
+            player.grenades -= 1
+            grenade_thrown = True
+
         if player.in_air:
             player.update_action(2)     # 2: jump
         elif moving_left or moving_right:
@@ -245,6 +294,8 @@ while run:
                 moving_right = True
             if event.key == pygame.K_SPACE:
                 shoot = True
+            if event.key == pygame.K_q:
+                grenade = True
             if event.key == pygame.K_w and player.alive:
                 player.jump = True
             if event.key == pygame.K_ESCAPE:
@@ -258,6 +309,9 @@ while run:
                 moving_right = False
             if event.key == pygame.K_SPACE:
                 shoot = False
+            if event.key == pygame.K_q:
+                grenade = False
+                grenade_thrown = False
 
     pygame.display.update()
 
